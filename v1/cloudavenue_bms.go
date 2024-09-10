4583dc9868b395e77cb1381ba2fdc4e3878cfcea
David MICHENEAU <david.micheneau@orange.com>
2024-09-10 17:01:40 +0200

    feat: Add endpoint bms

--- v1/cloudavenue_bms.go
+++ v1/cloudavenue_bms.go
@@ -7,56 +7,57 @@ import (
 	commoncloudavenue "github.com/orange-cloudavenue/cloudavenue-sdk-go/pkg/common/cloudavenue"
 )
 
-// type BMS struct{}
+type BMS struct{}
 
-type BMSs struct {
-	Networks []Network `json:"BMSnetworkDetails"`
-	BMSs     []BMS     `json:"BMSDetails"`
-}
+/*
+	type BMSs struct {
+		Networks []Network `json:"BMSnetworkDetails"`
+		BMSs     []BMS     `json:"BMSDetails"`
+	}
 
-type Network struct {
-	VLANID string `json:"vlanId"`
-	Subnet string `json:"subnet"`
-	Prefix string `json:"prefix"`
-}
+	type Network struct {
+		VLANID string `json:"vlanId"`
+		Subnet string `json:"subnet"`
+		Prefix string `json:"prefix"`
+	}
 
-type BMS struct {
-	BMSType           string    `json:"bmsType"`
-	Hostname          string    `json:"hostname"`
-	OS                string    `json:"os"`
-	BiosConfiguration string    `json:"biosConfiguration"`
-	Storage           []Storage `json:"storage"`
-}
+	type BMS struct {
+		BMSType           string    `json:"bmsType"`
+		Hostname          string    `json:"hostname"`
+		OS                string    `json:"os"`
+		BiosConfiguration string    `json:"biosConfiguration"`
+		Storage           []Storage `json:"storage"`
+	}
+	type Storage struct {
+		Local  []StorageDetails `json:"local"`
+		Shared []StorageDetails `json:"shared"`
+		System []StorageDetails `json:"system"`
+		Data   []StorageDetails `json:"data"`
+	}
 
-type Storage struct {
-	Local  []StorageDetails `json:"local"`
-	Shared []StorageDetails `json:"shared"`
-	System []StorageDetails `json:"system"`
-	Data   []StorageDetails `json:"data"`
-}
+	type StorageDetails struct {
+		StorageClass string `json:"storageClass"`
+		Size         string `json:"size"`
+	}
+*/
 
-type StorageDetails struct {
-	StorageClass string `json:"storageClass"`
-	Size         string `json:"size"`
-}
-
-func (v *BMS) List() (BMSs, error) {
+func (v *BMS) List() (BMS, error) {
 	c, err := clientcloudavenue.New()
 	if err != nil {
-		return BMSs{}, err
+		return BMS{}, err
 	}
 
 	r, err := c.R().
-		SetResult(&BMSs{}).
+		SetResult(&BMS{}).
 		SetError(&commoncloudavenue.APIErrorResponse{}).
 		Get("/api/customers/v2.0/bms")
 	if err != nil {
-		return BMSs{}, err
+		return BMS{}, err
 	}
 
 	if r.IsError() {
-		return BMSs{}, fmt.Errorf("error on list BMS(s): %s", r.Error().(*commoncloudavenue.APIErrorResponse).FormatError())
+		return BMS{}, fmt.Errorf("error on list BMS(s): %s", r.Error().(*commoncloudavenue.APIErrorResponse).FormatError())
 	}
 
-	return *r.Result().(*BMSs), nil
+	return *r.Result().(*BMS), nil
 }
