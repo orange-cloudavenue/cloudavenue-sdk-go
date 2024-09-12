ceda6234be2abde73b7be7506172de345a641a59
David MICHENEAU <david.micheneau@orange.com>
2024-09-12 16:45:49 +0200

    feat: Add bms and fix json part

--- v1/cloudavenue_bms.go
+++ v1/cloudavenue_bms.go
@@ -1,63 +1,134 @@
 package v1
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 
 	clientcloudavenue "github.com/orange-cloudavenue/cloudavenue-sdk-go/pkg/clients/cloudavenue"
 	commoncloudavenue "github.com/orange-cloudavenue/cloudavenue-sdk-go/pkg/common/cloudavenue"
 )
 
-type BMS struct{}
+type (
+	// BMS struct{}
 
-/*
-	type BMSs struct {
-		Networks []Network `json:"BMSnetworkDetails"`
-		BMSs     []BMS     `json:"BMSDetails"`
+	BMS struct {
+		BMSNetworks []BMSNetwork `json:"BMSnetworkDetails"`
+		BMSDetails  []BMSDetail  `json:"BMSDetails"`
 	}
 
-	type Network struct {
+	BMSNetwork struct {
 		VLANID string `json:"vlanId"`
 		Subnet string `json:"subnet"`
 		Prefix string `json:"prefix"`
 	}
 
-	type BMS struct {
-		BMSType           string    `json:"bmsType"`
-		Hostname          string    `json:"hostname"`
-		OS                string    `json:"os"`
-		BiosConfiguration string    `json:"biosConfiguration"`
-		Storage           []Storage `json:"storage"`
-	}
-	type Storage struct {
-		Local  []StorageDetails `json:"local"`
-		Shared []StorageDetails `json:"shared"`
-		System []StorageDetails `json:"system"`
-		Data   []StorageDetails `json:"data"`
+	BMSDetail struct {
+		BMSType           string     `json:"bmsType"`
+		Hostname          string     `json:"hostname"`
+		OS                string     `json:"os"`
+		BiosConfiguration string     `json:"biosConfiguration"`
+		Storages          BMSStorage `json:"storages"`
 	}
 
-	type StorageDetails struct {
+	BMSStorage struct {
+		Local  BMSStorageDetail `json:"local"`
+		Shared BMSStorageDetail `json:"shared"`
+		System BMSStorageDetail `json:"system"`
+		Data   BMSStorageDetail `json:"data"`
+	}
+
+	BMSStorageDetail struct {
 		StorageClass string `json:"storageClass"`
 		Size         string `json:"size"`
 	}
-*/
+)
 
-func (v *BMS) List() (BMS, error) {
+// ! BMS
+// function List BMSolution
+func (v *BMS) List() (response *BMS, err error) {
 	c, err := clientcloudavenue.New()
 	if err != nil {
-		return BMS{}, err
+		return nil, err
 	}
 
 	r, err := c.R().
-		SetResult(&BMS{}).
+		SetDoNotParseResponse(true).
+		// SetResult(&BMS{}). - because the response is not a correct json format
 		SetError(&commoncloudavenue.APIErrorResponse{}).
 		Get("/api/customers/v2.0/bms")
 	if err != nil {
-		return BMS{}, err
+		return nil, err
+	}
+	rep, err := io.ReadAll(r.RawBody())
+	if err != nil {
+		return nil, err
+	}
+	// ! convert []byte to string to fix the json format response
+	stringRep := string(rep)
+	// delete last character "]"
+	stringRep = stringRep[:len(stringRep)-1]
+	// delete first character "["
+	stringRep = stringRep[1:]
+
+	// put the json format y into struct *BMS
+	err = json.Unmarshal([]byte(stringRep), &response)
+	if err != nil {
+		return nil, err
 	}
 
 	if r.IsError() {
-		return BMS{}, fmt.Errorf("error on list BMS(s): %s", r.Error().(*commoncloudavenue.APIErrorResponse).FormatError())
+		return response, fmt.Errorf("error on list BMS(s): %s", r.Error().(*commoncloudavenue.APIErrorResponse).FormatError())
 	}
 
-	return *r.Result().(*BMS), nil
+	return response, nil
+	// return r.Result().(*BMS), nil
+}
+
+func (v *BMS) GetNetworks() []BMSNetwork {
+	return v.BMSNetworks
+}
+
+func (v *BMS) GetBMSDetails() []BMSDetail {
+	return v.BMSDetails
+}
+
+func (v *BMSDetail) GetBMSStorage() BMSStorage {
+	return v.Storages
+}
+
+func (v *BMSStorage) GetLocal() BMSStorageDetail {
+	return v.Local
+}
+
+func (v *BMSStorage) GetShared() BMSStorageDetail {
+	return v.Shared
+}
+
+func (v *BMSStorage) GetSystem() BMSStorageDetail {
+	return v.System
+}
+
+func (v *BMSStorage) GetData() BMSStorageDetail {
+	return v.Data
+}
+
+func (v *BMSStorageDetail) GetStorageClass() string {
+	return v.StorageClass
+}
+
+func (v *BMSStorageDetail) GetSize() string {
+	return v.Size
+}
+
+func (v *BMSNetwork) GetVLANID() string {
+	return v.VLANID
+}
+
+func (v *BMSNetwork) GetSubnet() string {
+	return v.Subnet
+}
+
+func (v *BMSNetwork) GetPrefix() string {
+	return v.Prefix
 }
