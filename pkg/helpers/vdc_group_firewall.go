78b3df962d4403e92865c693abefec62cdd1de45
Mickael Stanislas <contact@mickael-stanislas.com>
2024-12-02 15:44:40 +0100

    feat: add vdc group firewall (#153)

---
+++ pkg/helpers/vdc_group_firewall.go
@@ -0,0 +1,54 @@
+package helpers
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/orange-cloudavenue/cloudavenue-sdk-go/pkg/errors"
+	v1 "github.com/orange-cloudavenue/cloudavenue-sdk-go/v1"
+)
+
+// ParseVDCGroupFirewallTypeRuleDirection parses the string and returns the corresponding firewall rule direction.
+// If the string is not valid, it returns an error.
+func ParseVDCGroupFirewallRuleDirection(direction string) (v1.VDCGroupFirewallTypeRuleDirection, error) {
+	switch strings.ToLower(direction) {
+	case "in":
+		return v1.VDCGroupFirewallTypeRuleDirectionIn, nil
+	case "out":
+		return v1.VDCGroupFirewallTypeRuleDirectionOut, nil
+	case "inout", "in_out", "in-out":
+		return v1.VDCGroupFirewallTypeRuleDirectionInOut, nil
+	default:
+		return "", fmt.Errorf("%w. Use one of %v", errors.ErrInvalidFirewallRuleDirection, v1.VDCGroupFirewallTypeRuleDirections)
+	}
+}
+
+// ParseVDCGroupFirewallRuleIPProtocol parses the string and returns the corresponding firewall rule IP protocol.
+// If the string is not valid, it returns an error.
+func ParseVDCGroupFirewallRuleIPProtocol(protocol string) (v1.VDCGroupFirewallTypeRuleIPProtocol, error) {
+	switch strings.ToLower(protocol) {
+	case "ipv4":
+		return v1.VDCGroupFirewallTypeRuleIPProtocolIPv4, nil
+	case "ipv6":
+		return v1.VDCGroupFirewallTypeRuleIPProtocolIPv6, nil
+	case "ipv4-ipv6", "ipv4_ipv6", "ipv4ipv6":
+		return v1.VDCGroupFirewallTypeRuleIPProtocolIPv4IPv6, nil
+	default:
+		return "", fmt.Errorf("%w. Use one of %v", errors.ErrInvalidFirewallRuleIPProtocol, v1.VDCGroupFirewallTypeRuleIPProtocols)
+	}
+}
+
+// ParseVDCGroupFirewallRuleAction parses the string and returns the corresponding firewall rule action.
+// If the string is not valid, it returns an error.
+func ParseVDCGroupFirewallRuleAction(action string) (v1.VDCGroupFirewallTypeRuleAction, error) {
+	switch strings.ToLower(action) {
+	case "allow":
+		return v1.VDCGroupFirewallTypeRuleActionAllow, nil
+	case "drop":
+		return v1.VDCGroupFirewallTypeRuleActionDrop, nil
+	case "reject":
+		return v1.VDCGroupFirewallTypeRuleActionReject, nil
+	default:
+		return "", fmt.Errorf("%w. Use one of %v", errors.ErrInvalidFirewallRuleAction, v1.VDCGroupFirewallTypeRuleActions)
+	}
+}
---
+++ v1/vdc_group_distributed_firewall_types.go
@@ -0,0 +1,114 @@
+package v1
+
+import (
+	"github.com/vmware/go-vcloud-director/v2/govcd"
+	govcdtypes "github.com/vmware/go-vcloud-director/v2/types/v56"
+)
+
+type (
+	VDCGroupFirewall struct {
+		vdcGroup *govcd.VdcGroup
+		vgf      *govcd.DistributedFirewall
+	}
+
+	VDCGroupFirewallType struct {
+		Enabled bool
+
+		// Rules contains a list of firewall rules.
+		Rules VDCGroupFirewallTypeRules
+	}
+
+	VDCGroupFirewallTypeRules          []*VDCGroupFirewallTypeRule
+	VDCGroupFirewallTypeRuleDirection  string
+	VDCGroupFirewallTypeRuleIPProtocol string
+	VDCGroupFirewallTypeRuleAction     string
+
+	VDCGroupFirewallTypeRule struct {
+		// Required
+		Name    string
+		Enabled bool
+
+		// Direction 'IN_OUT', 'OUT', 'IN'.
+		//
+		// Use helpers.ParseVDCGroupFirewallTypeRuleDirection to parse string to VDCGroupFirewallTypeRuleDirection
+		Direction VDCGroupFirewallTypeRuleDirection
+
+		// IpProtocol 'IPV4', 'IPV6', 'IPV4_IPV6'.
+		//
+		// Use helpers.ParseVDCGroupFirewallTypeRuleIPProtocol to parse string to VDCGroupFirewallTypeRuleIPProtocol
+		IPProtocol VDCGroupFirewallTypeRuleIPProtocol
+
+		// Action defines action to be applied to all the traffic that meets the firewall rule criteria.
+		// It determines if the rule permits or blocks traffic.
+		// Use helpers.ParseVDCGroupFirewallTypeRuleAction to parse string to VDCGroupFirewallTypeRuleAction
+		//
+		// - ALLOW permits traffic to go through the firewall.
+		//
+		// - DROP blocks the traffic at the firewall. No response is sent back to the source.
+		//
+		// - REJECT blocks the traffic at the firewall. A response is sent back to the source.
+		Action VDCGroupFirewallTypeRuleAction
+
+		// Optional
+		ID          string
+		Description string // Length cannot exceed 2048 characters.
+		Logging     bool
+
+		// ApplicationPortProfiles contains a list of references to Application Port Profiles. Empty
+		// list means 'Any'
+		ApplicationPortProfiles []govcdtypes.OpenApiReference
+
+		// SourceFirewallGroups contains a list of references to Firewall Groups. Empty list means 'Any'
+		SourceFirewallGroups []govcdtypes.OpenApiReference
+		// DestinationFirewallGroups contains a list of references to Firewall Groups. Empty list means
+		// 'Any'
+		DestinationFirewallGroups []govcdtypes.OpenApiReference
+
+		// SourceGroupsExcluded reverses the list specified in SourceFirewallGroups and the rule gets
+		// applied on all the groups that are NOT part of the SourceFirewallGroups. If false, the rule
+		// applies to the all the groups including the source groups.
+		SourceGroupsExcluded *bool
+
+		// DestinationGroupsExcluded reverses the list specified in DestinationFirewallGroups and the
+		// rule gets applied on all the groups that are NOT part of the DestinationFirewallGroups. If
+		// false, the rule applies to the all the groups in DestinationFirewallGroups.
+		DestinationGroupsExcluded *bool
+	}
+)
+
+const (
+	VDCGroupFirewallTypeRuleDirectionInOut VDCGroupFirewallTypeRuleDirection = "IN_OUT"
+	VDCGroupFirewallTypeRuleDirectionOut   VDCGroupFirewallTypeRuleDirection = "OUT"
+	VDCGroupFirewallTypeRuleDirectionIn    VDCGroupFirewallTypeRuleDirection = "IN"
+
+	VDCGroupFirewallTypeRuleIPProtocolIPv4     VDCGroupFirewallTypeRuleIPProtocol = "IPV4"
+	VDCGroupFirewallTypeRuleIPProtocolIPv6     VDCGroupFirewallTypeRuleIPProtocol = "IPV6"
+	VDCGroupFirewallTypeRuleIPProtocolIPv4IPv6 VDCGroupFirewallTypeRuleIPProtocol = "IPV4_IPV6"
+
+	// Permits traffic to go through the firewall.
+	VDCGroupFirewallTypeRuleActionAllow VDCGroupFirewallTypeRuleAction = "ALLOW"
+	// Blocks the traffic at the firewall. No response is sent back to the source.
+	VDCGroupFirewallTypeRuleActionDrop VDCGroupFirewallTypeRuleAction = "DROP"
+	// Blocks the traffic at the firewall. A response is sent back to the source.
+	VDCGroupFirewallTypeRuleActionReject VDCGroupFirewallTypeRuleAction = "REJECT"
+)
+
+var (
+	VDCGroupFirewallTypeRuleDirections = []VDCGroupFirewallTypeRuleDirection{
+		VDCGroupFirewallTypeRuleDirectionInOut,
+		VDCGroupFirewallTypeRuleDirectionOut,
+		VDCGroupFirewallTypeRuleDirectionIn,
+	}
+
+	VDCGroupFirewallTypeRuleIPProtocols = []VDCGroupFirewallTypeRuleIPProtocol{
+		VDCGroupFirewallTypeRuleIPProtocolIPv4,
+		VDCGroupFirewallTypeRuleIPProtocolIPv6,
+		VDCGroupFirewallTypeRuleIPProtocolIPv4IPv6,
+	}
+
+	VDCGroupFirewallTypeRuleActions = []VDCGroupFirewallTypeRuleAction{
+		VDCGroupFirewallTypeRuleActionAllow,
+		VDCGroupFirewallTypeRuleActionDrop,
+		VDCGroupFirewallTypeRuleActionReject,
+	}
+)
