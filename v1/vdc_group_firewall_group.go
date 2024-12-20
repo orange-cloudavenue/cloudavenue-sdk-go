30cefae0767cdaca9d11a3ec8be0063dbba52042
Mickael Stanislas <contact@mickael-stanislas.com>
2024-12-20 16:16:53 +0100

    feat: add funcs for dynamic security group

--- v1/vdc_group_firewall_group.go
+++ v1/vdc_group_firewall_group.go
@@ -144,21 +144,91 @@ func (g VDCGroup) genericGetFirewallIPSet(nameOrID string) (*govcd.NsxtFirewallG
 
 // GetFirewallIPSet retrieves the IP set configuration for the VDC Group.
 func (g VDCGroup) GetFirewallIPSet(nameOrID string) (*FirewallGroupIPSet, error) {
-	vv, err := g.genericGetFirewallIPSet(nameOrID)
+	v, err := g.genericGetFirewallIPSet(nameOrID)
 	if err != nil {
 		return nil, err
 	}
 
 	return &FirewallGroupIPSet{
-		fwGroup: vv,
+		fwGroup: v,
 		FirewallGroupIPSetModel: &FirewallGroupIPSetModel{
 			FirewallGroupModel: FirewallGroupModel{
-				ID:          vv.NsxtFirewallGroup.ID,
-				Name:        vv.NsxtFirewallGroup.Name,
-				Description: vv.NsxtFirewallGroup.Description,
+				ID:          v.NsxtFirewallGroup.ID,
+				Name:        v.NsxtFirewallGroup.Name,
+				Description: v.NsxtFirewallGroup.Description,
 			},
-			IPAddresses: vv.NsxtFirewallGroup.IpAddresses,
+			IPAddresses: v.NsxtFirewallGroup.IpAddresses,
 		},
 		vg: g,
+	}, nil
+}
+
+// * Dynamic Security Group
+
+// CreateFirewallDynamicSecurityGroup allow creating a new dynamic security group for the VDC Group.
+func (g VDCGroup) CreateFirewallDynamicSecurityGroup(dynamicSecurityGroupConfig *FirewallGroupDynamicSecurityGroupModel) (*FirewallGroupDynamicSecurityGroup, error) {
+	if dynamicSecurityGroupConfig == nil {
+		return nil, fmt.Errorf("dynamicSecurityGroupConfig is nil")
+	}
+
+	if err := dynamicSecurityGroupConfig.Validate(); err != nil {
+		return nil, fmt.Errorf("validation error: %w", err)
+	}
+
+	v, err := g.vg.CreateNsxtFirewallGroup(dynamicSecurityGroupConfig.toGovcdtypesNsxtFirewallGroup(g.GetID(), g.GetName()))
+	if err != nil {
+		return nil, err
+	}
+
+	dynamicSecurityGroupConfig.fromGovcdtypesNsxtFirewallGroup(v.NsxtFirewallGroup)
+
+	return &FirewallGroupDynamicSecurityGroup{
+		fwGroup:                                v,
+		FirewallGroupDynamicSecurityGroupModel: dynamicSecurityGroupConfig,
+		vg:                                     g,
+	}, nil
+}
+
+func (g VDCGroup) genericGetFirewallDynamicSecurityGroup(nameOrID string) (*govcd.NsxtFirewallGroup, error) {
+	var values *govcd.NsxtFirewallGroup
+
+	if nameOrID == "" {
+		return nil, fmt.Errorf("the name or ID must be provided")
+	}
+
+	err := retry.Do(
+		func() error {
+			var err error
+			if urn.IsSecurityGroup(nameOrID) {
+				values, err = g.vg.GetNsxtFirewallGroupById(nameOrID)
+			} else {
+				values, err = g.vg.GetNsxtFirewallGroupByName(nameOrID, govcdtypes.FirewallGroupTypeVmCriteria)
+			}
+
+			return err
+		},
+		retry.RetryIf(func(err error) bool {
+			return err != nil && strings.Contains(err.Error(), "could not find NSX-T Firewall Group")
+		}),
+		retry.Attempts(5),
+	)
+
+	return values, err
+}
+
+// GetFirewallDynamicSecurityGroup retrieves the dynamic security group configuration for the VDC Group.
+func (g VDCGroup) GetFirewallDynamicSecurityGroup(nameOrID string) (*FirewallGroupDynamicSecurityGroup, error) {
+	v, err := g.genericGetFirewallDynamicSecurityGroup(nameOrID)
+	if err != nil {
+		return nil, err
+	}
+
+	x := &FirewallGroupDynamicSecurityGroupModel{}
+	x.fromGovcdtypesNsxtFirewallGroup(v.NsxtFirewallGroup)
+
+	return &FirewallGroupDynamicSecurityGroup{
+		fwGroup:                                v,
+		FirewallGroupDynamicSecurityGroupModel: x,
+		vg:                                     g,
 	}, nil
 }
