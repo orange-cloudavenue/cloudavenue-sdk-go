475c8bccb1a36b5dcedd7263257ef7cde0e172c8
Mickael Stanislas <contact@mickael-stanislas.com>
2025-02-03 10:17:14 +0100

    fix: org properties

--- v1/org/properties_types.go
+++ v1/org/properties_types.go
@@ -15,7 +15,7 @@ type (
 		Email        string `json:"customerMail,omitempty" validate:"omitempty,email"`
 		Description  string `json:"description,omitempty" validate:"omitempty"`
 		FullName     string `json:"fullName,omitempty" validate:"omitempty"`
-		BillingModel string `json:"internetBillingMode" validate:"required,oneof=PAYG TRAFFIC_VOLUME"`
+		BillingModel string `json:"internetBillingMode,omitempty" validate:"omitempty,oneof=PAYG TRAFFIC_VOLUME"`
 	}
 
 	PropertiesRequest PropertiesModel
