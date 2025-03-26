a2cb3d8e7ee214a1d7ed2398cd3302e48b5b7511
Mickael Stanislas <contact@mickael-stanislas.com>
2025-03-26 14:26:44 +0100

    feat: add funcs network routed VDC/VDCG (#233)

--- v1/vdc_network.go
+++ v1/vdc_network.go
@@ -13,9 +13,12 @@ import (
 	"github.com/avast/retry-go/v4"
 
 	"github.com/vmware/go-vcloud-director/v2/govcd"
+	govcdtypes "github.com/vmware/go-vcloud-director/v2/types/v56"
 
 	"github.com/orange-cloudavenue/cloudavenue-sdk-go/pkg/urn"
 )
+
+// * Isolated Network
 
 // GetNetworkIsolated returns the isolated network by its name or ID.
 func (v *VDC) GetNetworkIsolated(nameOrID string) (*VDCNetworkIsolated, error) {
@@ -34,6 +37,68 @@ func (v *VDC) GetNetworkIsolated(nameOrID string) (*VDCNetworkIsolated, error) {
 
 	x.data.fromVDCNetworkModel(net.OpenApiOrgVdcNetwork)
 	x.VDCNetworkIsolatedModel = x.data
+	return x, nil
+}
+
+// CreateNetworkIsolated creates an isolated network.
+func (v *VDC) CreateNetworkIsolated(model *VDCNetworkIsolatedModel) (*VDCNetworkIsolated, error) {
+	net, err := v.createVDCNetwork(model.toVDCNetworkModel(v, govcdtypes.OrgVdcNetworkTypeIsolated))
+	if err != nil {
+		return nil, err
+	}
+
+	x := &VDCNetworkIsolated{
+		VDCNetwork: VDCNetwork[*VDCNetworkIsolatedModel]{
+			v:    v,
+			net:  net,
+			data: &VDCNetworkIsolatedModel{},
+		},
+	}
+
+	x.data.fromVDCNetworkModel(net.OpenApiOrgVdcNetwork)
+	x.VDCNetworkIsolatedModel = x.data
+	return x, nil
+}
+
+// * Routed Network
+
+// GetNetworkRouted returns the routed network by its name or ID.
+func (v *VDC) GetNetworkRouted(nameOrID string) (*VDCNetworkRouted, error) {
+	net, err := v.genericGetNetwork(nameOrID)
+	if err != nil {
+		return nil, err
+	}
+
+	x := &VDCNetworkRouted{
+		VDCNetwork: VDCNetwork[*VDCNetworkRoutedModel]{
+			v:    v,
+			net:  net,
+			data: &VDCNetworkRoutedModel{},
+		},
+	}
+
+	x.data.fromVDCNetworkModel(net.OpenApiOrgVdcNetwork)
+	x.VDCNetworkRoutedModel = x.data
+	return x, nil
+}
+
+// CreateNetworkRouted creates a routed network.
+func (v *VDC) CreateNetworkRouted(model *VDCNetworkRoutedModel) (*VDCNetworkRouted, error) {
+	net, err := v.createVDCNetwork(model.toVDCNetworkModel(v, govcdtypes.OrgVdcNetworkTypeRouted))
+	if err != nil {
+		return nil, err
+	}
+
+	x := &VDCNetworkRouted{
+		VDCNetwork: VDCNetwork[*VDCNetworkRoutedModel]{
+			v:    v,
+			net:  net,
+			data: &VDCNetworkRoutedModel{},
+		},
+	}
+
+	x.data.fromVDCNetworkModel(net.OpenApiOrgVdcNetwork)
+	x.VDCNetworkRoutedModel = x.data
 	return x, nil
 }
 
@@ -58,22 +123,20 @@ func (v VDC) genericGetNetwork(nameOrID string) (*govcd.OpenApiOrgVdcNetwork, er
 	return values, err
 }
 
-// CreateNetworkIsolated creates an isolated network.
-func (v *VDC) CreateNetworkIsolated(model *VDCNetworkIsolatedModel) (*VDCNetworkIsolated, error) {
-	net, err := v.createVDCNetwork(model.toVDCNetworkModel(v))
-	if err != nil {
-		return nil, err
-	}
+// IsRouted returns true if the network is routed.
+func (v VDCNetwork[T]) IsRouted() bool {
+	return v.net.IsRouted()
+}
 
-	x := &VDCNetworkIsolated{
-		VDCNetwork: VDCNetwork[*VDCNetworkIsolatedModel]{
-			v:    v,
-			net:  net,
-			data: &VDCNetworkIsolatedModel{},
-		},
-	}
+// IsIsolated returns true if the network is isolated.
+func (v VDCNetwork[T]) IsIsolated() bool {
+	return v.net.IsIsolated()
+}
 
-	x.data.fromVDCNetworkModel(net.OpenApiOrgVdcNetwork)
-	x.VDCNetworkIsolatedModel = x.data
-	return x, nil
+// IsShared returns true if the network is shared.
+func (v VDCNetwork[T]) IsShared() bool {
+	if v.net.OpenApiOrgVdcNetwork.Shared == nil {
+		return false
+	}
+	return *v.net.OpenApiOrgVdcNetwork.Shared
 }
